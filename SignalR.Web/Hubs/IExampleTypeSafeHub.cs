265d25255ad20a627df05558e9d6dae6cb39c1b2
oZIem <oaakboyraz@gmail.com>
2024-11-06 14:28:26 +0300

    signalr

---
+++ SignalR.Web/Hubs/IExampleTypeSafeHub.cs
@@ -0,0 +1,26 @@
+﻿using SignalR.Web.Models;
+
+namespace SignalR.Web.Hubs
+{
+    public interface IExampleTypeSafeHub
+    {
+        Task ReceiveMessageForAllClient(string message);
+
+        Task ReceiveTypedMessageToAllClient(Product product);
+
+
+        Task ReceiveConnectedClientCountAllClient(int clientCount);
+
+
+        Task ReceiveMessageForCallerClient(string message);
+
+        Task ReceiveMessageForOthersClient(string message);
+
+        Task ReceiveMessageForIndividualClient(string message);
+
+        Task ReceiveMessageForGroupClients(string message);
+    
+    }
+    
+}
+
---
+++ SignalR.Web/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalR.Web.Models
+{
+
+    //burası sadece data yani property tutacaksa class değil de record olsun
+    public record Product(int Id, string Name, decimal Price) //.net 8 primary constructor
+    {
+
+       
+
+    }
+}
+
+
